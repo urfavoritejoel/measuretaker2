be5c40d7e05f53b8374295c8c50dccd7aad146a5
Joel Motes <yourfavoritejoel@gmail.com>
2025-04-26 22:03:09 -0500

    ....

--- measuretaker2/frontend/src/context/Modal.jsx
+++ measuretaker2/frontend/src/context/Modal.jsx
@@ -24,7 +24,7 @@ export function ModalProvider({ children }) {
     modalRef, // reference to modal div
     modalContent, // React component to render inside modal
     setModalContent, // function to set the React component to render inside modal
-    setOnModalClose, // function to set the callback function called when modal is closing...
+    setOnModalClose, // function to set the callback function called when modal is closing....
     closeModal // function to close the modal
   };
 
