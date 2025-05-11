999942c645b2d1a76a2429825fe738435eac07ee
Joel Motes <yourfavoritejoel@gmail.com>
2025-05-10 20:26:34 -0500

    .

--- measuretaker2/frontend/src/context/Modal.jsx
+++ measuretaker2/frontend/src/context/Modal.jsx
@@ -12,7 +12,7 @@ export function ModalProvider({ children }) {
 
   const closeModal = () => {
     setModalContent(null); // clear the modal contents
-    // If callback function is truthy, call the callback function and reset it
+    // If callback function is truthy, call the callback function and reset it.
     // to null:
     if (typeof onModalClose === 'function') {
       setOnModalClose(null);
@@ -25,7 +25,7 @@ export function ModalProvider({ children }) {
     modalContent, // React component to render inside modal
     setModalContent, // function to set the React component to render inside modal
     setOnModalClose, // function to set the callback function called when modal is closing
-    closeModal // function to close the modal.....
+    closeModal // function to close the modal
   };
 
   return (
